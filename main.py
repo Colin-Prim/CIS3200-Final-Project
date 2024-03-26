4ff16b3ebc748d24c024b8ad8e86f78798c9c6ec
Colin-Prim <cprim@umich.edu>
2024-03-26 13:53:14 -0400

    loading everything

--- main.py
+++ main.py
@@ -8,4 +8,23 @@ import matplotlib.pyplot as plt
 with open("wikivital_mathematics.json") as f:
     data = json.load(f)
 
-print(data.keys())
+edges = np.array(data['edges'])
+weights = np.array(data['weights'])
+weighted_edges = [edge + [weight] for edge, weight in zip(data['edges'], data['weights'])]
+
+time_periods = {}
+for time_period in range(data['time_periods']):
+    time_periods[time_period] = data[str(time_period)]
+dates = pd.DataFrame.from_dict(time_periods, orient="index", columns=['year', 'month', 'day'])
+
+daily_visits = pd.DataFrame.from_dict(time_periods, orient="index", columns=['y'])
+
+pages_pos = pd.DataFrame.from_dict(data['node_ids'], orient='index')
+pages_pos.columns = ["pos"]
+pages_pos = pages_pos.sort_values("pos")
+
+pos_pages = pd.DataFrame(pages_pos.index, index=pages_pos.pos)
+pos_pages.columns = ['page']
+
+daily_visits = pd.DataFrame(daily_visits.y.to_list())
+daily_visits = daily_visits.set_index(pd.to_datetime(dates))
