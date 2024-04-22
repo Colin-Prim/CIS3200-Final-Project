0796bc2f4b6676a7cf93effe82acf01d9e998317
Colin-Prim <cprim@umich.edu>
2024-04-21 22:47:22 -0400

    New weighting systems

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ daily_visits = pd.DataFrame(daily_visits.y.to_list())
 daily_visits = daily_visits.set_index(pd.to_datetime(dates))
 
 total_visits = daily_visits.sum(axis=0)
+avg_total_visits = total_visits.mean()
 
 #  allows finding of position (pos) from page name
 #  sample syntax: pages_pos.loc['Mathematics'].pos yields 0
@@ -47,21 +48,119 @@ pages_pos = pages_pos.sort_values("pos")
 pos_pages = pd.DataFrame(pages_pos.index, index=pages_pos.pos)
 pos_pages.columns = ['page']
 
+
+'''Default Weighting (Num Recurrences)'''
 #  init graph from weighted_edges
 G = nx.DiGraph()
 G.add_weighted_edges_from(weighted_edges)
 
 #  rank pages and convert to a numpy array
 ranked = nx.pagerank(G)
-ranked_array = np.array(list(ranked.items()))
-ranked_df = pd.DataFrame.from_dict(ranked, orient="index")
-# print(ranked_array)  # comment out to speed up program
 
+new_sorted = sorted(ranked.items(), key=lambda item: item[1], reverse=False)
+new_array = np.array(list(new_sorted))
+
+print("Weighted by Number of Duplicate Edges:")
+n = 0
+while n < 10:
+    index = new_array[n][0]
+    print(pos_pages.loc[index].page)
+    n += 1
+
+
+'''Weighting by Total Visitation of Incoming Node'''
 new_weighted_edges = [(from_node, to_node, total_visits.get(to_node, np.nan)) for from_node, to_node in edges]
 
 G_new = nx.DiGraph()
 G_new.add_weighted_edges_from(new_weighted_edges)
 
 new_ranked = nx.pagerank(G_new)
-new_ranked_array = np.array(list(new_ranked.items()))
-# print(new_ranked_array)  # comment out to speed up program
+
+new_sorted = sorted(new_ranked.items(), key=lambda item: item[1], reverse=False)
+new_array = np.array(list(new_sorted))
+
+print("\n" + "Weighted by Total Visitation (Incoming):")
+n = 0
+while n < 10:
+    index = new_array[n][0]
+    print(pos_pages.loc[index].page)
+    n += 1
+
+
+'''Weighting by Total Visitation of Outgoing Node'''
+new_weighted_edges = [(from_node, to_node, total_visits.get(from_node, np.nan)) for from_node, to_node in edges]
+
+G_new = nx.DiGraph()
+G_new.add_weighted_edges_from(new_weighted_edges)
+
+new_ranked = nx.pagerank(G_new)
+
+new_sorted = sorted(new_ranked.items(), key=lambda item: item[1], reverse=True)
+new_array = np.array(list(new_sorted))
+
+print("\n" + "Weighted by Total Visitation (Outgoing):")
+n = 0
+while n < 10:
+    index = new_array[n][0]
+    print(pos_pages.loc[index].page)
+    n += 1
+
+
+'''Weighting by Avg Total Visitation of Incoming and Outgoing Nodes'''
+new_weighted_edges = [(from_node, to_node, (total_visits.get(from_node, np.nan) + total_visits.get(to_node, np.nan))
+                       / 2) for from_node, to_node in edges]
+
+G_new = nx.DiGraph()
+G_new.add_weighted_edges_from(new_weighted_edges)
+
+new_ranked = nx.pagerank(G_new)
+
+new_sorted = sorted(new_ranked.items(), key=lambda item: item[1], reverse=True)
+new_array = np.array(list(new_sorted))
+
+print("\n" + "Weighted by Avg Total Visitation (Incoming/Outgoing):")
+n = 0
+while n < 10:
+    index = new_array[n][0]
+    print(pos_pages.loc[index].page)
+    n += 1
+
+'''This method of formatting produces a graph on which the PageRank algorithm does not converge'''
+# '''Weighting by Total Visitation of Incoming Node Minus Average Total Visitation'''
+# new_weighted_edges = [(from_node, to_node, total_visits.get(to_node, np.nan) - avg_total_visits)
+#                       for from_node, to_node in edges]
+#
+# G_new = nx.DiGraph()
+# G_new.add_weighted_edges_from(new_weighted_edges)
+#
+# new_ranked = nx.pagerank(G_new)
+#
+# new_sorted = sorted(new_ranked.items(), key=lambda item: item[1], reverse=True)
+# new_array = np.array(list(new_sorted))
+#
+# print("\n" + "Weighted by Total Visitation (Incoming) Minus Average Visitation:")
+# n = 0
+# while n < 10:
+#     index = new_array[n][0]
+#     print(pos_pages.loc[index].page)
+#     n += 1
+
+
+'''Weighting by Total Visitation of Outgoing Node Minus Average Total Visitation'''
+new_weighted_edges = [(from_node, to_node, total_visits.get(from_node, np.nan) - avg_total_visits)
+                      for from_node, to_node in edges]
+
+G_new = nx.DiGraph()
+G_new.add_weighted_edges_from(new_weighted_edges)
+
+new_ranked = nx.pagerank(G_new)
+
+new_sorted = sorted(new_ranked.items(), key=lambda item: item[1], reverse=True)
+new_array = np.array(list(new_sorted))
+
+print("\n" + "Weighted by Total Visitation (Outgoing) Minus Average Visitation:")
+n = 0
+while n < 10:
+    index = new_array[n][0]
+    print(pos_pages.loc[index].page)
+    n += 1
